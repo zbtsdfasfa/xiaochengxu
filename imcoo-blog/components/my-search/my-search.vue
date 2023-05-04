50536e97bc702d09456ade6d38e2f73fa0655742
zbtsdfasfa <129084016+zbtsdfasfa@users.noreply.github.com>
2023-05-04 14:29:17 +0800

    慕课热搜搜索区域

---
+++ imcoo-blog/components/my-search/my-search.vue
@@ -0,0 +1,52 @@
+<template>
+	<!-- 搜索组件 -->
+	<view class="my-search-container">
+		<!-- 搜索按钮 -->
+		<view class="my-search-box">
+			<!-- 搜索图片 -->
+			<img class="icon" src="@/static/images/search.png" alt="搜索图标">
+			<!-- 文本内容 -->
+			<text class="placeholder">{{ placeholderText }}</text>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-search",
+		props:{
+			placeholderText:{
+				type:String,
+				default:"搜索"
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.my-search-container {
+		// background-color: red;	
+		display: flex;
+		align-items: center;
+		.my-search-box {
+			display: flex;
+			align-items: center;
+			height: 35px;
+			width: 100%;
+			border: 1px solid gray;
+			border-radius: 10px;
+			.icon {
+				display: fle;
+				align-items: center;
+				width: 20px;
+				height: 20px;
+			}
+		}
+	}
+</style>
---
+++ imcoo-blog/pages/hot/hot.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="hot-container">
+		<!-- logo图片区域 -->
+		<img src="@/static/images/logo.png" alt="慕课热搜logo" class="logo" mode="aspectFit">
+		<!-- 自定义组件可以直接是使用 -->
+		<view class="search-box">
+			<my-search placeholder-text="uni-app自定义组件"></my-search>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.hot-container {
+	background-color: $uni-bg-color;
+	.logo{
+		width: 100%;
+		height: 80px;
+	}
+}
+.search-box {
+	padding: 0 14px;
+	margin-bottom: 8px;
+}
+</style>
